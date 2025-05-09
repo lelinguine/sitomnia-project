846ac183a22b5cbec51c5b9ebb309444a2651e1d
Valentin <luginbuhlvalentin@gmail.com>
2025-05-09 11:41:52 +0200

    update

--- docs/endpoints.md
+++ docs/endpoints.md
@@ -1,2 +1,16 @@
 [Retour](../readme.md)
 # Projet-TER/Endpoints
+
+## Base
+- **GET** `/` : Informations des services.
+  - **Sorties** : 
+    - `api_version` : Numéro de version.
+    - `fastapi_version` : Numéro de version de fastapi.
+    - `model` : Modèle de l'intelligence artificielle utilisée.
+
+## Model
+  - **POST** `/ask` : Envoyer un prompt au server ollama.
+    - **Entrées** : 
+      - `prompt` (string, requis) : Description du prompt.
+    - **Sorties** : 
+      - `response` : Réponse au prompt envoyé.
--- services/main.py
+++ services/main.py
@@ -1,24 +1,32 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 import requests
+import fastapi
 
 app = FastAPI()
 
-OLLAMA_URL = "http://localhost:11434/api/generate"
+OLLAMA_URL = "http://localhost:11434/api/chat"
 MODEL_NAME = "llama3.2:3b"
+API_VERSION = "1.0.0-a"
 
 class PromptRequest(BaseModel):
     prompt: str
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {
+        "api_version": API_VERSION,
+        "fastapi_version": fastapi.__version__,
+        "model": MODEL_NAME
+    }
 
 @app.post("/ask")
 def ask_model(request: PromptRequest):
     payload = {
         "model": MODEL_NAME,
-        "prompt": request.prompt,
+        "messages": [
+            {"role": "user", "content": request.prompt}
+        ],
         "stream": False
     }
 
@@ -26,6 +34,6 @@ def ask_model(request: PromptRequest):
         response = requests.post(OLLAMA_URL, json=payload)
         response.raise_for_status()
         data = response.json()
-        return {"response": data.get("response")}
+        return {"response": data.get("message", {}).get("content", "No content returned")}
     except requests.RequestException as e:
         raise HTTPException(status_code=500, detail=str(e))
