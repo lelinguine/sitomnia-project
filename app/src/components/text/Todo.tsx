5e30afb78ce7268d670c6207e36967a42fdd624b
Valentin <luginbuhlvalentin@gmail.com>
2025-05-21 12:44:39 +0200

    Update Todo.tsx

--- app/src/components/text/Todo.tsx
+++ app/src/components/text/Todo.tsx
@@ -3,20 +3,24 @@ import React from "react";
 import Icon from "../Icon";
 import "./text.css";
 
-const Todo = ({ children, isChecked, onClick } : { children : React.ReactNode }) => {
-      const classNameIsChecked = isChecked ? "checked-todo" : "";
-      const iconName = isChecked ? "CircleCheck" : "Circle";
+type TodoProps = {
+  children: React.ReactNode;
+  isChecked: boolean;
+  onClick: () => void;
+};
 
-      return (
-        <>
-            <div className="bubble-container" onClick={onClick}>
-                <div className={`bubble todo ${classNameIsChecked}`}>
-                    <Icon icon={iconName} size={24}/>
-                    <span className="md-text">{children}</span>
-                </div>
-            </div>
-        </>
-    );
+const Todo = ({ children, isChecked, onClick }: TodoProps) => {
+  const classNameIsChecked = isChecked ? "checked-todo" : "";
+  const iconName = isChecked ? "CircleCheck" : "Circle";
+
+  return (
+    <div className="bubble-container" onClick={onClick}>
+      <div className={`bubble todo ${classNameIsChecked}`}>
+        <Icon icon={iconName} size={24} />
+        <span className="md-text">{children}</span>
+      </div>
+    </div>
+  );
 };
 
 export default Todo;
